2219a993785b03c9711136ce8fe2b1f24cab17d0
Dayo <liyo2686@gmail.com>
2024-07-03 15:32:49 +0800

    create RegisterViewModel

---
+++ com.ftc.Blog/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace com.ftc.Blog.Models
+{
+    public class RegisterViewModel
+    {
+
+        [Display(Name = "帳號")]
+        public string Account { get; set; }
+        [Display(Name = "密碼")]
+        public string Password { get; set; }
+        [Display(Name = "電子信箱")]
+        public string Email { get; set; }
+
+    }
+}
