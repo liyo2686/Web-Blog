28ff9c0301c4c16d74805d1321a97a6957d86aff
Dayo <liyo2686@gmail.com>
2024-07-05 15:58:22 +0800

    新增發文功能，補刪舊資料庫檔案

---
+++ com.ftc.Blog/Controllers/ArticleController.cs
@@ -0,0 +1,49 @@
+﻿using com.ftc.Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.ftc.Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private WebBlogDBEntities1 db = new WebBlogDBEntities1();
+        // GET: Article
+        public ActionResult addArticle()
+        {
+            return View();
+        }
+
+        // 登入請求的 POST 方法
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult addArticle(Article_View_Model model)
+        {
+            if (ModelState.IsValid)
+            {
+                string currentUserAccount = User.Identity.Name;
+                var user = db.Users.FirstOrDefault(u => u.Account == currentUserAccount);
+                var article = new Articles
+                {
+                    Title = model.Title,
+                    Content = model.Content,
+                    UserID = user.UserID,
+                    CreatedTime = DateTime.Now
+                };
+                db.Articles.Add(article);
+                db.SaveChanges();
+                // 跳轉到文章列表
+                return RedirectToAction("Post", "Post");
+            }
+            else
+            {
+                ModelState.AddModelError("", "發生錯誤，無法新增文章：");
+            }
+
+            // 如果模型驗證失敗，返回原始的新增文章的視圖，並顯示驗證錯誤訊息
+            return View(model);
+        }
+    }
+}
