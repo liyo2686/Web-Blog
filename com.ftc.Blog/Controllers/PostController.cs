79b96fb0cd2b7b658d68b6410a7538dceaa54408
Dayo <liyo2686@gmail.com>
2024-07-05 15:56:05 +0800

    修改登出圖片、註冊完成後url導向

--- com.ftc.Blog/Controllers/PostController.cs
+++ com.ftc.Blog/Controllers/PostController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using com.ftc.Blog.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +14,6 @@ namespace com.ftc.Blog.Controllers
         {
             return View();
         }
+   
     }
 }
