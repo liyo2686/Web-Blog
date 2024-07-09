9d245f2a99e00a14637498198c6a64f10bf7d5e8
Dayo <liyo2686@gmail.com>
2024-07-09 15:33:36 +0800

    新增分頁功能

--- com.ftc.Blog/Controllers/PostController.cs
+++ com.ftc.Blog/Controllers/PostController.cs
@@ -1,10 +1,12 @@
 ﻿using com.ftc.Blog.Models;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Web.UI;
 
 
 namespace com.ftc.Blog.Controllers
@@ -13,7 +15,7 @@ namespace com.ftc.Blog.Controllers
     {
         private WebBlogDBEntities1 db = new WebBlogDBEntities1();
         // GET: Post
-        public ActionResult Post(string searchString)
+        public ActionResult Post(string searchString, int? page)
         {
             int currentUserId = GetCurrentUserIdFromCookie();
             ViewBag.CurrentUserId = currentUserId; // 或者使用 ViewData 傳遞
@@ -31,7 +33,15 @@ namespace com.ftc.Blog.Controllers
                 articles = articles.Where(a => a.Title.Contains(searchString)).ToList();
             }
 
-            return View(articles.OrderByDescending(a => a.CreatedTime));
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            IPagedList<Article_View_Model> pagedArticles = articles.OrderByDescending(a => a.CreatedTime).ToPagedList(pageNumber, pageSize);
+
+            //return View(articles.OrderByDescending(a => a.CreatedTime));
+            return View(pagedArticles);
+            //articles = articles.OrderByDescending(a => a.CreatedTime).ToPagedList(pageNumber, pageSize);
+
         }
 
         private int GetCurrentUserIdFromCookie()
